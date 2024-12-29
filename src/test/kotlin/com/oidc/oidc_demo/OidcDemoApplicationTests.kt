00988c9e65670b0004d2e0e767567be05603b6c0
北沢亮和 <ryowa.kitazawa@gmail.com>
2024-12-29 17:07:36 +0900

    first commit

---
+++ src/test/kotlin/com/oidc/oidc_demo/OidcDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.oidc.oidc_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OidcDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
